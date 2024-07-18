0bd90ea73d4a8511c67b19d35cbd535e89be7729
marco.grimaudo <171530064+mrcgrm@users.noreply.github.com>
2024-07-18 13:07:27 +0900

    CONRAT-34866 chore: using relative paths for href links

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -913,7 +913,7 @@ function initializeIndex() {
   const logout = document.createElement('li');
   logout.setAttribute('id', 'logout-link');
   const logoutlink = document.createElement('a');
-  logoutlink.setAttribute('href', '/logout.html');
+  logoutlink.setAttribute('href', 'logout.html');
   logoutlink.setAttribute(
     'class',
     'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
@@ -924,7 +924,7 @@ function initializeIndex() {
   const login = document.createElement('li');
   login.setAttribute('id', 'login-link');
   const loginlink = document.createElement('a');
-  loginlink.setAttribute('href', '/login.html');
+  loginlink.setAttribute('href', 'login.html');
   loginlink.setAttribute(
     'class',
     'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
